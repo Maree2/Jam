d4248025904c87c5b41c19f676a14a45738fa29b
Jorge Palacios <pctroll@gmail.com>
2015-01-24 15:48:05 -0430

    Added VisualStudio Tools for Unity

---
+++ JamProject/Assets/Core/Scripts/ActionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionController : MonoBehaviour
+{
+
+
+    // Awake is called when the script instance is being loaded (Since v1.0)
+    public void Awake()
+    {
+
+    }
+
+    // OnBecameVisible is called when the renderer became visible by any camera (Since v1.0)
+    public void OnBecameInvisible()
+    {
+
+    }
+
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled (Since v1.0)
+    public void FixedUpdate()
+    {
+
+    }
+}
