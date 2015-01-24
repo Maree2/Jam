963c4dd8a1d761fe1e4baf92eb74d73cd3cf0c62
Jorge Palacios <pctroll@gmail.com>
2015-01-24 16:49:06 -0430

    Input camera background color

--- JamProject/Assets/Core/Scripts/ActionController.cs
+++ JamProject/Assets/Core/Scripts/ActionController.cs
@@ -3,17 +3,137 @@ using System.Collections;
 
 public class ActionController : MonoBehaviour
 {
+
+    public Camera mainCamera;
+    public float pizzicatoThreshold = 0.36f;
+
+    public Color earthColor;
+    public Color fireColor;
+    public Color waterColor;
+    public Color windColor;
+    private Color defaultColor;
+
+    private bool isEarth;
+    private bool isEarthOld;
+
+    private bool isFire;
+    private bool isFireOld;
+
+    private bool isWater;
+    private bool isWaterOld;
+
+    private bool isWind;
+    private bool isWindOld;
+
+    private float earthTime;
+    private float fireTime;
+    private float waterTime;
+    private float windTime;
+
     public void Start()
     {
-
+        if (mainCamera == null)
+            Debug.LogError("No camera assigned");
+        defaultColor = mainCamera.backgroundColor;
     }
 
     public void Update()
     {
-        
-    }
 
-    public void Awake()
-    {
+        #region button validation
+
+        if (Input.GetButtonDown("Earth"))
+        {
+            mainCamera.backgroundColor = earthColor;
+            isEarth = true;
+        }
+
+        if (Input.GetButtonUp("Earth"))
+        {
+            earthTime = 0f;
+            isEarth = false;
+            mainCamera.backgroundColor = defaultColor;
+        }
+
+        if (Input.GetButtonDown("Fire"))
+        {
+            mainCamera.backgroundColor = fireColor;
+            isFire = true;
+        }
+
+        if (Input.GetButtonUp("Fire"))
+        {
+            fireTime = 0f;
+            isFire = false;
+            mainCamera.backgroundColor = defaultColor;
+        }
+
+        if (Input.GetButtonDown("Water"))
+        {
+            mainCamera.backgroundColor = waterColor;
+            isWater = true;
+        }
+
+        if (Input.GetButtonUp("Water"))
+        {
+            if (waterTime <= pizzicatoThreshold)
+            {
+                // TODO
+                // call pizzicato here no loop
+            }
+            else
+            {
+                // TODO
+                // stop playing the loop
+            }
+            waterTime = 0f;
+            isWater = false;
+            mainCamera.backgroundColor = defaultColor;
+        }
+
+        if (Input.GetButtonDown("Wind"))
+        {
+            mainCamera.backgroundColor = windColor;
+            isWind = true;
+        }
+
+        if (Input.GetButtonUp("Wind"))
+        {
+            if (windTime <= pizzicatoThreshold)
+            {
+                // TODO
+                // call pizzicato here no loop
+            }
+            else
+            {
+                // TODO
+                // stop playing the loop
+            }
+            windTime = 0f;
+            isWind = false;
+            mainCamera.backgroundColor = defaultColor;
+        }
+        #endregion
+
+        if (isEarth)
+            earthTime += Time.deltaTime;
+        if (isFire)
+            fireTime += Time.deltaTime;
+        if (isWater)
+            waterTime += Time.deltaTime;
+        if (isWind)
+            windTime += Time.deltaTime;
+
+        if (waterTime > pizzicatoThreshold)
+        {
+            // TODO
+            // play indefinitely
+        }
+
+        if (windTime > pizzicatoThreshold)
+        {
+            // TODO
+            // play indefinitely
+        }
     }
 }
