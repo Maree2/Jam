464ad2b3378bf4ddf29c51d1074954e05b1492cd
Jorge Palacios <pctroll@gmail.com>
2015-01-24 15:56:13 -0430

    Work on ActionController

--- JamProject/Assets/Core/Scripts/ActionController.cs
+++ JamProject/Assets/Core/Scripts/ActionController.cs
@@ -3,22 +3,17 @@ using System.Collections;
 
 public class ActionController : MonoBehaviour
 {
+    public void Start()
+    {
 
+    }
 
-    // Awake is called when the script instance is being loaded (Since v1.0)
+    public void Update()
+    {
+        
+    }
+
     public void Awake()
-    {
-
-    }
-
-    // OnBecameVisible is called when the renderer became visible by any camera (Since v1.0)
-    public void OnBecameInvisible()
-    {
-
-    }
-
-    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled (Since v1.0)
-    public void FixedUpdate()
     {
 
     }
