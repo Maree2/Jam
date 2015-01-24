3947684d4d2d6e49c2ab5d2918b12bae02c6b2d5
Christian Chomiak <christianchomiak@gmail.com>
2015-01-24 17:33:53 -0430

    Planet 1.0

---
+++ JamProject/Assets/Core/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+    [SerializeField]
+    Transform center, lookat;
+
+    [SerializeField]
+    float distanceFromCenter;
+
+    [SerializeField]
+    [Range(0, 360)]
+    float angleX, angleY, lookAngle;
+
+    void Awake()
+    { 
+        
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = CalculatePosition(angleX, angleY);
+        Camera.main.transform.LookAt(center, transform.up);
+        Camera.main.transform.Rotate(Vector3.right, -lookAngle);
+	}
+
+    Vector3 CalculatePosition(float angleInX, float angleInY)
+    {
+        //angleInX -= 360f;
+        angleInX *= Mathf.Deg2Rad;
+        angleInY *= Mathf.Deg2Rad;
+
+        float x = center.position.x + Mathf.Sin(angleInX) * distanceFromCenter * Mathf.Cos(angleInY);
+        float y = center.position.y + Mathf.Cos(angleInX) * distanceFromCenter;
+        float z = center.position.z + Mathf.Sin(angleInX) * distanceFromCenter * Mathf.Sin(angleInY);
+
+        return new Vector3(x, y, z);
+    }
+}
---
+++ JamProject/Assets/Core/Scripts/PlanetController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Mesh planetMesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = planetMesh.vertices;
+
+        Color[] colors = new Color[vertices.Length];
+        float m;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            m = vertices[i].magnitude;
+            vertices[i] += planetMesh.normals[i] * Mathf.PerlinNoise(vertices[i].x, vertices[i].y) * 0.2f;
+            float diff = vertices[i].magnitude - m;
+
+            /*if (diff >= 0 && diff < 0.05f)
+            {
+                colors[i] = Color.blue;
+            }
+            else if (diff >= 0.05f && diff < 0.15f)
+            {
+                colors[i] = Color.Lerp(Color.blue, Color.green,  1 - (0.15f - diff) / (0.15f - 0.05f)) ;
+            }
+            else if (diff >= 0.15f && diff < 0.18f)
+            {
+                colors[i] = Color.green;
+            }
+            else if (diff >= 0.18f && diff < 0.22f)
+            {
+                colors[i] = Color.Lerp(Color.green, Color.white, 1 - (0.22f - diff) / (0.22f - 0.18f));
+            }
+            else*/
+            colors[i] = Color.Lerp(Color.green, Color.blue, 1 - (0.2f - diff) / (0.2f)); //Color.green;
+
+        }
+
+        planetMesh.colors = colors;
+
+        planetMesh.vertices = vertices;
+
+        planetMesh.RecalculateNormals();
+        planetMesh.RecalculateBounds();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
