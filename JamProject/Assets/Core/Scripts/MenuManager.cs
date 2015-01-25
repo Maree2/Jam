8ec9387bad63c10eaed22eb8d61beade4e01655a
Christian Chomiak <christianchomiak@gmail.com>
2015-01-25 14:50:32 -0430

    Menu and other scenes done

---
+++ JamProject/Assets/Core/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetButtonDown("Earth"))
+        {
+            Application.LoadLevel("SimpleJamLevel");
+        }
+        else if (Input.GetButtonDown("Wind"))
+        {
+            Application.LoadLevel("JamLevel");
+        }
+        else if (Input.GetButtonDown("Fire"))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetButtonDown("Water"))
+        {
+            Application.LoadLevel("CreditsLevel");
+        }
+	}
+}
---
+++ JamProject/Assets/Core/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReturnToMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.LoadLevel("MenuLevel");
+        }
+	}
+}
